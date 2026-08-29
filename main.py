776b545ae92fb42537b5dc766c835a63e508168f
cupcakegirl2026 <liamnyaks@gmail.com>
2026-08-29 09:41:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+print("I am Ethel")
+#this is a comment, I love Python
+#print multiple lines
+print("I am learning Python")
+print("Python is not fun")
+
+print("I am Python\ni am learning Python")
